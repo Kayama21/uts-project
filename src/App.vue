e67c4a19f20505bdc363c8b89a48df1ffd21256e
Night Raven <kharismaarya21@gmail.com>
2020-03-19 17:38:37 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <div class="header-top pt-4 bg-color-2">
+      <div class="container p-3">
+        <div class="row">
+          <div class="header-logo mr-5 ml-5"><img src="./assets/logo.png" width="40px" height="30px"></div>
+            <div>
+              <router-link to="/">Home</router-link> |
+              <router-link to="/about">About</router-link>
+            </div>
+        </div>
+      </div>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,67 @@
+<template>
+<div class="main bg-color-1">
+        <div class="jumbo-tron">
+            <div class="hero-image"></div>
+        </div>
+
+        <div class="card-container">
+            <div class="row">
+                <div class="cardmenu-1">
+                    <h3>LARAVEL</h3><hr>
+                    <img src="../assets/logo.png" width="100" height="180">
+                </div>
+                <div class="cardmenu-1">
+                    <h3>LARAVEL</h3>
+                    <img src="../assets/logo.png" width="100" height="180">
+                </div>
+                <div class="cardmenu-1">
+                    <h3>LARAVEL</h3>
+                    <img src="../assets/logo.png" width="100" height="180">
+                </div>
+                <div class="cardmenu-1">
+                    <h3>LARAVEL</h3>
+                    <img src="../assets/logo.png" width="100" height="180">
+                </div>
+            </div>
+        </div>
+
+        <div class="card-container">
+            <div class="row">
+                <div class="cardmenu-2">
+                    <h4>Produk Terlaris</h4><hr>
+                    <img src="../assets/logo.png" width="100" height="50">
+                </div>
+            </div>
+        </div>
+
+        <div class="card-container">
+            <div class="cardmenu-3">
+                <h4>Produk 1</h4><hr>
+                <img src="../assets/logo.png" width="100" height="50">
+            </div>
+            <div class="cardmenu-3">
+                <h4>Produk 2</h4><hr>
+                <img src="../assets/logo.png" width="100" height="50">
+            </div>
+            <div class="cardmenu-3">
+                <h4>Produk 3</h4><hr>
+                <img src="../assets/logo.png" width="100" height="50">
+            </div>
+            <div class="cardmenu-3">
+                <h4>Produk 4</h4><hr>
+                <img src="../assets/logo.png" width="100" height="50">
+            </div>
+            <div class="cardmenu-3">
+                <h4>Produk 5</h4><hr>
+                <img src="../assets/logo.png" width="100" height="50">
+            </div>
+            <div class="cardmenu-3">
+                <h4>Produk 6</h4><hr>
+                <img src="../assets/logo.png" width="100" height="50">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
